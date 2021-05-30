2f5873f7281dfa5917a0ff4776a9eb5834a189f5
Joel C <joel@vorce.se>
2021-05-30 09:54:53 +0200

    Extract functions and add unit tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,16 +1,46 @@
-import * as assert from 'assert';
+import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as gotoTest from '../../extension';
+import * as vscode from "vscode";
+import * as gotoTest from "../../extension";
 
-suite('getTestFilePath', () => {
-	test('returns null for invalid source file URI scheme', () => {
-		const sourceFile = vscode.Uri.parse('http://www.msft.com/some/path');
+suite("fileNameWithoutExtension", () => {
+  test("returns uri file without its extension", () => {
+    const uri = vscode.Uri.file("./src/extension.ts");
 
-		const testFile = gotoTest.getTestFilePath(sourceFile);
-		
-		assert.strictEqual(testFile, null);
-	});
+    const result = gotoTest.fileNameWithoutExtension(uri, "ts");
+
+    assert.strictEqual(result, "extension");
+  });
+});
+
+suite("getTestFileName", () => {
+  test("returns ts test file", () => {
+    const uri = vscode.Uri.file("./src/extension.ts");
+
+    const result = gotoTest.getTestFileName(uri);
+
+    assert.strictEqual(result, "extension.test.ts");
+  });
+});
+
+suite("getTestFilePath", () => {
+  test("returns null for invalid source file URI scheme", () => {
+    const sourceFile = vscode.Uri.parse("http://www.msft.com/some/path");
+
+    const testFile = gotoTest.getTestFilePath(sourceFile);
+
+    assert.strictEqual(testFile, null);
+  });
+});
+
+suite("defaultTestFilePattern", () => {
+  test("contains source file pattern", () => {
+    const ending = "exs";
+
+    const result = gotoTest.defaultTestFilePattern(ending);
+
+    assert.strictEqual(result.includes("%source_file%"), true);
+  });
 });
